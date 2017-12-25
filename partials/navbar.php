cab0a84c0b6bd2ba47ad663752c1437580c6f293
vritant24 <vritoku@gmail.com>
2017-12-25 16:21:40 -0500

    moble-nav top done

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,14 +1,92 @@
  <!-- Navigation Bar -->
  <nav class="nav">
-        <!-- the spans are for vertical alignment -->
-    <div class="nav-logo"><span></span>PSSC</div>
+    <!-- the spans are for vertical alignment -->
+    <div class="nav-logo">
+        <span class="nav-valign"></span>
+        PSSC
+    </div>
+
     <ul class="nav-bar">
-        <li class="nav-bar-item nav-bar-item-active"><span></span><a class="nav-bar-item-a" href="/">Home</a></li>
-        <li class="nav-bar-item"><span></span><a class="nav-bar-item-a" href="/about">About</a></li>
-        <li class="nav-bar-item"><span></span><a class="nav-bar-item-a" href="/calendar">Calendar</a></li>
-        <li class="nav-bar-item"><span></span><a class="nav-bar-item-a" href="/apply">Apply</a></li>
-        <li class="nav-bar-item"><span></span><a class="nav-bar-item-a" href="/outstanding-teacher-award">Outstanding Teacher Award</a></li>
-        <li class="nav-bar-item"><span></span><a class="nav-bar-item-a" href="/contact">Contact</a></li>
+        <li class="nav-bar-item nav-bar-item-active">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/">
+                Home
+            </a>
+        </li>
+        <li class="nav-bar-item">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/about">
+                About
+            </a>
+        </li>
+        <li class="nav-bar-item">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/calendar">
+                Calendar
+            </a>
+        </li>
+        <li class="nav-bar-item">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/apply">
+                Apply
+            </a>
+        </li>
+        <li class="nav-bar-item">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/outstanding-teacher-award">
+                Outstanding Teacher Award
+            </a>
+        </li>
+        <li class="nav-bar-item">
+            <span class="nav-valign"></span>
+            <a class="nav-bar-item-a" href="/contact">
+                Contact
+            </a>
+        </li>
     </ul>
+</nav>
+<nav class="m-nav">
+    <div class="m-nav-top">
+        <div class="m-nav-top-item">
+            <span class="m-nav-top-item-valign"></span>
+            <span class="m-nav-top-item-logo">PSSC</span>
+        </div>
+        <div class="m-nav-top-item">
+            <span class="m-nav-top-item-valign"></span>
+            <span class="m-nav-top-item-hamburger">H</span>
+        </div>
+    </div>
+    <div class="m-nav-bar">
+        <li class="m-nav-bar-item m-nav-bar-item-active">
+            <a class="m-nav-bar-item-a" href="/">
+                Home
+            </a>
+        </li>
+        <li class="m-nav-bar-item">
+            <a class="m-nav-bar-item-a" href="/about">
+                About
+            </a>
+        </li>
+        <li class="m-nav-bar-item">
+            <a class="m-nav-bar-item-a" href="/calendar">
+                Calendar
+            </a>
+        </li>
+        <li class="m-nav-bar-item">
+            <a class="m-nav-bar-item-a" href="/apply">
+                Apply
+            </a>
+        </li>
+        <li class="m-nav-bar-item">
+            <a class="m-nav-bar-item-a" href="/outstanding-teacher-award">
+                Outstanding Teacher Award
+            </a>
+        </li>
+        <li class="m-nav-bar-item">
+            <a class="m-nav-bar-item-a" href="/contact">
+                Contact
+            </a>
+        </li>
+    </div>
 </nav>
 <!-- ---------- -->
