b92fb1b3d796192da806ebd0c387e9551f7ee72b
vritant24 <vritoku@gmail.com>
2017-12-27 19:29:11 -0500

    apply page done for now

---
+++ apply.php
@@ -0,0 +1,16 @@
+<?php include("partials/header.php") ?>
+    <main class="main-container">
+        <div class="page">
+            <h1 class="page-title">Apply</h1>
+            <hr class="page-divider">
+
+            <p class="page-text">
+                Applications are currently closed.
+            </p>
+            <p class="page-text">
+                Please feel free to show interest in our 
+                <a class="page-text-a" href="https://boilerlink.purdue.edu/organization/purduesciencestudentcouncil">get involved page</a>
+            </p>
+    </main>
+    <script src="js/landing.js"></script>
+<?php include("partials/footer.php") ?>
