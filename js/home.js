127c5ccd4490496f9f1180a7c0b9ddd882650510
Jeonghu <park955@purdue.edu>
2020-02-21 13:16:07 -0500

    Home page hamburger icon fixed

--- js/home.js
+++ js/home.js
@@ -23,12 +23,12 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "inline-flex"; 
-  dots[slideIndex-1].className += " active";
+  //slides[slideIndex-1].style.display = "inline-flex"; 
+  //dots[slideIndex-1].className += " active";
 }
 
 window.onload = function() {
-    addClickListener("js-m-nav-click", navDrawer);
+  addClickListener("js-m-nav-click", navDrawer);
 };
 
 function navDrawer() {
