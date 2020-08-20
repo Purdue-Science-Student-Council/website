1813572341164f4c06c452980e040484628d8cb0
Jeonghu Park <park955@purdue.edu>
2020-08-19 21:43:50 -0500

    NSO mobile menu fix

---
+++ js/nso.js
@@ -0,0 +1,14 @@
+function navDrawer() {
+    var m_nav = document.getElementById("js-m-nav");
+    m_nav.classList.toggle("m-nav-active");
+};
+
+function addClickListener(id, func) {
+    document.getElementById(id).addEventListener("click", func); 
+};
+
+
+window.onload = function() {
+    addClickListener("js-m-nav-click", navDrawer);
+};
+
