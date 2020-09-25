f4cc7b289e6ab7c9177a027db67a57d7bf4e8f7c
Jeonghu Park <park955@purdue.edu>
2020-09-25 06:50:13 -0400

    Meeting minute update 09/23

--- js/meetingminutes.js
+++ js/meetingminutes.js
@@ -1,13 +1,15 @@
 const dates_fa2020 = [
     "08-26-20",
     "09-02-20",
-    "09-09-20"
+    "09-09-20",
+    "09-23-20"
 ];
 
 const links_fa2020 = [
     "https://docs.google.com/document/d/e/2PACX-1vTUBLiHuNWioxrb_3-sblhh8ltthhpufsVgHfAoN4piDShipioshOlxqFRFTfwR8vdPEJ9ZCoFuF_DW/pub",
     "https://docs.google.com/document/d/e/2PACX-1vQNryxIV25WUypepbVDtPMJTUECadGgJWTPmG8AzunBV74PYMrtarG0U4kJTfjsPzYqRMXoUddz1eFh/pub",
-    "https://docs.google.com/document/d/e/2PACX-1vRHLo6wQMUCdT6gu3DqwJJB7y5XuJE5Iuh2z5QvYxu9szHu-LApxfuz3V1bPz_Gj4YWG4DsvMW2uf_b/pub"
+    "https://docs.google.com/document/d/e/2PACX-1vRHLo6wQMUCdT6gu3DqwJJB7y5XuJE5Iuh2z5QvYxu9szHu-LApxfuz3V1bPz_Gj4YWG4DsvMW2uf_b/pub",
+    "https://docs.google.com/document/d/e/2PACX-1vQjyU9IRAW-0pYeY3ca-SPTY3kxrqLpouQlYtoV0MVn9Lgd3F1k6kTRvJH2WrLoLMs8GrrOSWOHJR8o/pub"
 ]
 
 const dates_sp2020 = [
