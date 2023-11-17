8cfe5da54a4d942785faaf0f40198c141bab134a
Pramey Kabra <kabrap@purdue.edu>
2023-11-17 12:54:07 -0500

    Add Contributors Page + Add to Navbar

---
+++ js/contributors.js
@@ -0,0 +1,41 @@
+const contributors = [
+    "Vritant Bhardwaj",
+    "Jeonghu Park",
+    "Jeonghu Park",
+    "Tom Appenzeller",
+    "Kaitlynn Tran",
+    "Pramey Kabra",
+];
+const beginning_year = 2018;
+
+function addPreviousContributors() {
+    var table = document.getElementById("js-contributors-table");
+
+    const str = contributors.reduce((acc, contributor, index) => {
+        const temp = 
+            '<tr class="page-table-tr">' + 
+                '<td class="page-table-td">' + (beginning_year + index) + '</td>' +
+                '<td class="page-table-td">' + contributor + '</td>' +
+            '<tr>';
+        acc = temp + acc;
+        return acc;
+    }, "");
+
+    table.insertAdjacentHTML("beforeend", str);
+};
+
+function navDrawer() {
+    var m_nav = document.getElementById("js-m-nav");
+    m_nav.classList.toggle("m-nav-active");
+};
+
+function addClickListener(id, func) {
+    document.getElementById(id).addEventListener("click", func); 
+};
+
+
+window.onload = function() {
+    addClickListener("js-m-nav-click", navDrawer);
+    addPreviousContributors();
+};
+
